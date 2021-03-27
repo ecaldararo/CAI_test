4dcba499e35afe8a91aa0b68775f6a2b995066a5
ecaldararo <ecaldararo@gmail.com>
2021-03-26 21:37:37 -0300

    test 2

---
+++ Clases/Clases.Library1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases.Library1
+{
+    public class TextValidator
+    {
+        public static string Start()
+        {
+            return "Hola";
+        }
+
+        public static bool ValidarLongitudMaxima(string palabra, int LongitudMaxima)
+        {
+            return (palabra.Length > LongitudMaxima);
+        }
+
+        public void SetModelo(string elModelo)
+        {
+            if (string.IsNullOrEmpty(elModelo))
+            {
+                elModelo = " ";
+            }
+            else
+            {
+                elModelo = elModelo.ToUpper();
+            }
+        }
+    }
+}
--- Clases/Clases.PrimeraClase/Program.cs
+++ Clases/Clases.PrimeraClase/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Clases.Library1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Clases.PrimeraClase
 {
@@ -10,7 +12,16 @@ namespace Clases.PrimeraClase
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ingrese una palabra de menos de 4 letras");
+            string palabra = Console.ReadLine();
 
+            if (TextValidator.ValidarLongitudMaxima(palabra, 4))
+                Console.WriteLine("Superó o igual");
+            else
+                Console.WriteLine("No superó");
+
+
+            Console.ReadKey();
         }
     }
 }
